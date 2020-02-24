4fa105ec58cb199a0fb12f36b57513593cf87a0a
Antony <1307444320@qq.com>
2020-02-24 15:58:46 +0800

    MediaExtractor分离 音/视 频道生成新的多媒体文件

--- antony_codec/src/main/java/com/codec/androidmedia/MediaExtractorActivity.java
+++ antony_codec/src/main/java/com/codec/androidmedia/MediaExtractorActivity.java
@@ -7,6 +7,7 @@ import android.media.MediaExtractor;
 import android.media.MediaFormat;
 import android.media.MediaMuxer;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -22,6 +23,8 @@ public class MediaExtractorActivity extends AppCompatActivity {
     private final String TAG = "MediaExtractorActivity";
     MediaExtractor mMediaExtractor = null;
     MediaMuxer mMediaMuxer = null;
+    public final static String AUDIO_MIME = "audio";
+    public final static String VIDEO_MIME = "video";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,13 +35,23 @@ public class MediaExtractorActivity extends AppCompatActivity {
         isolate_video_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        divideMedia(ResPath.Extractor_MP4_RES, VIDEO_MIME);
+                    }
+                }).start();
             }
         });
         isolate_audio_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        divideMedia(ResPath.Extractor_MP4_RES, AUDIO_MIME);
+                    }
+                }).start();
             }
         });
         /*new Thread(new Runnable() {
@@ -98,5 +111,120 @@ public class MediaExtractorActivity extends AppCompatActivity {
         mMediaMuxer.stop();
         mMediaMuxer.release();
         return true;
+    }
+
+    public void divideMedia(String sourceMediaPath, String divideMime) {
+        mMediaExtractor = new MediaExtractor();
+        try {
+            mMediaExtractor.setDataSource(sourceMediaPath);
+            int count = mMediaExtractor.getTrackCount();
+            for (int i = 0; i < count; i++) {
+                MediaFormat mediaFormat = mMediaExtractor.getTrackFormat(i);
+                String mime = mediaFormat.getString(MediaFormat.KEY_MIME);
+                if (!TextUtils.isEmpty(mime) && !mime.startsWith(divideMime)) {
+                    continue;
+                }
+                int inputSize = mediaFormat.getInteger(MediaFormat.KEY_MAX_INPUT_SIZE);
+                ByteBuffer byteBuffer = ByteBuffer.allocate(inputSize);
+                if (divideMime.equals(AUDIO_MIME)) {
+                    mMediaMuxer = new MediaMuxer(ResPath.Extractor_MP3_DST, MediaMuxer.OutputFormat.MUXER_OUTPUT_MPEG_4);
+                    int audioTrack = mMediaMuxer.addTrack(mediaFormat);
+                    mMediaMuxer.start();
+                    divideToOutputAudio(mMediaExtractor, mMediaMuxer, byteBuffer, mediaFormat, audioTrack, i);
+                    finish(mMediaExtractor, mMediaMuxer);
+                    break;
+                } else if (divideMime.equals(VIDEO_MIME)) {
+                    mMediaMuxer = new MediaMuxer(ResPath.Extractor_MP4_DST, MediaMuxer.OutputFormat.MUXER_OUTPUT_MPEG_4);
+                    int videoTrack = mMediaMuxer.addTrack(mediaFormat);
+                    mMediaMuxer.start();
+                    divideToOutputVideo(mMediaExtractor, mMediaMuxer, byteBuffer, mediaFormat, videoTrack, i);
+                    finish(mMediaExtractor, mMediaMuxer);
+                    break;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void divideToOutputVideo(MediaExtractor mediaExtractor, MediaMuxer mediaMuxer, ByteBuffer byteBuffer, MediaFormat mediaFormat, int videoTrack, int videoTrackIndex) {
+        long videoDuration = mediaFormat.getLong(MediaFormat.KEY_DURATION);
+        mediaExtractor.selectTrack(videoTrackIndex);
+        MediaCodec.BufferInfo bufferInfo = new MediaCodec.BufferInfo();
+        bufferInfo.presentationTimeUs = 0;
+        long videoFrameTimes;
+        mediaExtractor.readSampleData(byteBuffer, 0);
+        if (mediaExtractor.getSampleFlags() != MediaExtractor.SAMPLE_FLAG_SYNC) {
+            mediaExtractor.advance();
+        }
+        mediaExtractor.readSampleData(byteBuffer, 0);
+        mediaExtractor.advance();
+        long firstFrame = mediaExtractor.getSampleTime();
+        mediaExtractor.advance();
+        mediaExtractor.readSampleData(byteBuffer, 0);
+        long secondFrame = mediaExtractor.getSampleTime();
+
+        videoFrameTimes = Math.abs(secondFrame - firstFrame);
+        mediaExtractor.seekTo(0, MediaExtractor.SEEK_TO_CLOSEST_SYNC);
+        int sampleSize;
+        while ((sampleSize = mediaExtractor.readSampleData(byteBuffer, 0)) != -1) {
+            long presentTime = bufferInfo.presentationTimeUs;
+            if (presentTime >= videoDuration) {
+                mediaExtractor.unselectTrack(videoTrackIndex);
+                break;
+            }
+            mediaExtractor.advance();
+            bufferInfo.offset = 0;
+            bufferInfo.flags = mediaExtractor.getSampleFlags();
+            bufferInfo.size = sampleSize;
+            mediaMuxer.writeSampleData(videoTrack, byteBuffer, bufferInfo);
+            bufferInfo.presentationTimeUs += videoFrameTimes;
+        }
+        mediaExtractor.unselectTrack(videoTrackIndex);
+    }
+
+    private void divideToOutputAudio(MediaExtractor mediaExtractor, MediaMuxer mediaMuxer, ByteBuffer byteBuffer, MediaFormat format, int audioTrack, int audioTrackIndex) {
+        long audioSampleSize;
+        int sampleRate = format.getInteger(MediaFormat.KEY_SAMPLE_RATE);
+        int channelCount = format.getInteger(MediaFormat.KEY_CHANNEL_COUNT);
+        long audioDuration = format.getLong(MediaFormat.KEY_DURATION);
+        mediaExtractor.selectTrack(audioTrackIndex);
+        MediaCodec.BufferInfo bufferInfo = new MediaCodec.BufferInfo();
+        bufferInfo.presentationTimeUs = 0;
+        mediaExtractor.readSampleData(byteBuffer, 0);
+        if (mediaExtractor.getSampleTime() == 0) {
+            mediaExtractor.advance();
+        }
+        mediaExtractor.readSampleData(byteBuffer, 0);
+        long firstRateSample = mediaExtractor.getSampleTime();
+        mediaExtractor.advance();
+        mediaExtractor.readSampleData(byteBuffer, 0);
+        long secondRateSample = mediaExtractor.getSampleTime();
+        audioSampleSize = Math.abs(secondRateSample - firstRateSample);
+        mediaExtractor.seekTo(0, MediaExtractor.SEEK_TO_PREVIOUS_SYNC);
+        int sampleSize;
+        while ((sampleSize = mediaExtractor.readSampleData(byteBuffer, 0)) != -1) {
+            int trackIndex = mediaExtractor.getSampleTrackIndex();
+            long presentationTimeUs = bufferInfo.presentationTimeUs;
+            Log.d(TAG, "trackIndex:" + trackIndex + ",presentationTimeUs:" + presentationTimeUs);
+            if (presentationTimeUs >= audioDuration) {
+                mediaExtractor.unselectTrack(audioTrackIndex);
+                break;
+            }
+            mediaExtractor.advance();
+            bufferInfo.offset = 0;
+            bufferInfo.size = sampleSize;
+            mediaMuxer.writeSampleData(audioTrack, byteBuffer, bufferInfo);//audioTrack为通过mediaMuxer.add()获取到的
+            bufferInfo.presentationTimeUs += audioSampleSize;
+        }
+        mediaExtractor.unselectTrack(audioTrackIndex);
+    }
+
+    private void finish(MediaExtractor mediaExtractor, MediaMuxer mediaMuxer) {
+        mediaMuxer.stop();
+        mediaMuxer.release();
+        mediaMuxer = null;
+        mediaExtractor.release();
+        mediaExtractor = null;
     }
 }
