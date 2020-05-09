c38d9c3ff7a2f5bbf282596509db64b189a6e566
Antony <1307444320@qq.com>
2020-05-09 10:24:43 +0800

    添加资源文件， 构建opengl基类

---
+++ antony_opengl/src/main/java/app/antony/base/MyFboRender.java
@@ -0,0 +1,110 @@
+package com.ywl5320.openglesegl.base;
+
+import android.content.Context;
+import android.opengl.GLES20;
+
+import com.ywl5320.openglesegl.R;
+import com.ywl5320.openglesegl.base.ShaderUtil;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+
+public class MyFboRender {
+
+    private Context context;
+
+    private float[] vertexData = {
+            -1f, -1f,
+            1f, -1f,
+            -1f, 1f,
+            1f, 1f
+    };
+    private FloatBuffer vertexBuffer;
+
+    private float[] fragmentData = {
+            0f, 1f,
+            1f, 1f,
+            0f, 0f,
+            1f, 0f
+    };
+    private FloatBuffer fragmentBuffer;
+
+    private int program;
+    private int vPosition;
+    private int fPosition;
+    private int textureid;
+    private int sampler;
+
+    private int vboId;
+
+    public MyFboRender(Context context) {
+        this.context = context;
+
+        vertexBuffer = ByteBuffer.allocateDirect(vertexData.length * 4)
+                .order(ByteOrder.nativeOrder())
+                .asFloatBuffer()
+                .put(vertexData);
+        vertexBuffer.position(0);
+
+        fragmentBuffer = ByteBuffer.allocateDirect(fragmentData.length * 4)
+                .order(ByteOrder.nativeOrder())
+                .asFloatBuffer()
+                .put(fragmentData);
+        fragmentBuffer.position(0);
+
+    }
+
+    public void onCreate()
+    {
+        String vertexSource = ShaderUtil.getRawResource(context, R.raw.vertex_shader);
+        String fragmentSource = ShaderUtil.getRawResource(context, R.raw.fragment_shader);
+
+        program = ShaderUtil.createProgram(vertexSource, fragmentSource);
+
+        vPosition = GLES20.glGetAttribLocation(program, "v_Position");
+        fPosition = GLES20.glGetAttribLocation(program, "f_Position");
+        sampler = GLES20.glGetUniformLocation(program, "sTexture");
+
+        int [] vbos = new int[1];
+        GLES20.glGenBuffers(1, vbos, 0);
+        vboId = vbos[0];
+
+        GLES20.glBindBuffer(GLES20.GL_ARRAY_BUFFER, vboId);
+        GLES20.glBufferData(GLES20.GL_ARRAY_BUFFER, vertexData.length * 4 + fragmentData.length * 4, null, GLES20. GL_STATIC_DRAW);
+        GLES20.glBufferSubData(GLES20.GL_ARRAY_BUFFER, 0, vertexData.length * 4, vertexBuffer);
+        GLES20.glBufferSubData(GLES20.GL_ARRAY_BUFFER, vertexData.length * 4, fragmentData.length * 4, fragmentBuffer);
+        GLES20.glBindBuffer(GLES20.GL_ARRAY_BUFFER, 0);
+    }
+
+    public void onChange(int width, int height)
+    {
+        GLES20.glViewport(0, 0, width, height);
+    }
+
+    public void onDraw(int textureId)
+    {
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+        GLES20.glClearColor(1f,0f, 0f, 1f);
+
+        GLES20.glUseProgram(program);
+
+        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, textureId);
+
+
+        GLES20.glBindBuffer(GLES20.GL_ARRAY_BUFFER, vboId);
+
+        GLES20.glEnableVertexAttribArray(vPosition);
+        GLES20.glVertexAttribPointer(vPosition, 2, GLES20.GL_FLOAT, false, 8,
+                0);
+
+        GLES20.glEnableVertexAttribArray(fPosition);
+        GLES20.glVertexAttribPointer(fPosition, 2, GLES20.GL_FLOAT, false, 8,
+                vertexData.length * 4);
+
+        GLES20.glDrawArrays(GLES20.GL_TRIANGLE_STRIP, 0, 4);
+
+        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, 0);
+        GLES20.glBindBuffer(GLES20.GL_ARRAY_BUFFER, 0);
+    }
+}
---
+++ antony_opengl/src/main/java/app/antony/util/ShaderUtil.java
@@ -0,0 +1,78 @@
+package com.ywl5320.openglesegl.base;
+
+import android.content.Context;
+import android.opengl.GLES20;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class ShaderUtil {
+
+    public static String getRawResource(Context context, int rawId)
+    {
+        InputStream inputStream = context.getResources().openRawResource(rawId);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        StringBuffer sb = new StringBuffer();
+        String line;
+        try
+        {
+            while((line = reader.readLine()) != null)
+            {
+                sb.append(line).append("\n");
+            }
+            reader.close();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        return sb.toString();
+    }
+
+    private static int loadShader(int shaderType, String source)
+    {
+        int shader = GLES20.glCreateShader(shaderType);
+        if(shader != 0)
+        {
+            GLES20.glShaderSource(shader, source);
+            GLES20.glCompileShader(shader);
+
+            int[] compile = new int[1];
+            GLES20.glGetShaderiv(shader, GLES20.GL_COMPILE_STATUS, compile, 0);
+            if(compile[0] != GLES20.GL_TRUE)
+            {
+                Log.d("ywl5320", "shader compile error");
+                GLES20.glDeleteShader(shader);
+                shader = 0;
+            }
+            return shader;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
+    public static int createProgram(String vertexSource, String fragmentSoruce)
+    {
+        int vertexShader = loadShader(GLES20.GL_VERTEX_SHADER, vertexSource);
+        int fragmentShader = loadShader(GLES20.GL_FRAGMENT_SHADER, fragmentSoruce);
+
+        if(vertexShader != 0 && fragmentShader != 0)
+        {
+            int program = GLES20.glCreateProgram();
+
+            GLES20.glAttachShader(program, vertexShader);
+            GLES20.glAttachShader(program, fragmentShader);
+
+            GLES20.glLinkProgram(program);
+            return program;
+        }
+        return 0;
+    }
+
+
+
+}
